564794a301ed95559c30221f9236663627ac5419
aurialt <144707111+aurialt@users.noreply.github.com>
2024-07-28 17:33:04 -0500

    Added updater files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module org.example.updatertest {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.logging;
+    requires org.controlsfx.controls;
+    requires java.desktop;
 
     opens org.example.updatertest to javafx.fxml;
     exports org.example.updatertest;
+
+    exports org.example.updatertest.xr3playerupdater.application to javafx.graphics;
+
+    opens org.example.updatertest.xr3playerupdater.presenter to javafx.fxml;
 }
---
+++ src/main/java/org/example/updatertest/xr3playerupdater/tools/NotificationType.java
@@ -0,0 +1,28 @@
+/*
+ * 
+ */
+package org.example.updatertest.xr3playerupdater.tools;
+
+/**
+ * The type of the notification to be shown.
+ *
+ * @author GOXR3PLUS
+ */
+public enum NotificationType {
+
+    /** The simple. */
+    SIMPLE,
+
+    /** The warning. */
+    WARNING,
+
+    /** The information. */
+    INFORMATION,
+
+    /** The confirm. */
+    CONFIRM,
+
+    /** The error. */
+    ERROR;
+
+}
