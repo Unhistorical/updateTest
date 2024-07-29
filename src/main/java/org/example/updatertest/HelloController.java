7eed99df4b589e00f258b124a9ba5331b1deb73f
aurialt <144707111+aurialt@users.noreply.github.com>
2024-07-29 17:04:54 -0500

    Added method to move files to specific destination

--- src/main/java/org/example/updatertest/HelloController.java
+++ src/main/java/org/example/updatertest/HelloController.java
@@ -1,9 +1,6 @@
 package org.example.updatertest;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import org.example.updatertest.xr3playerupdater.application.Main;
-import org.example.updatertest.xr3playerupdater.presenter.DownloadModeController;
 
 public class HelloController {
     @FXML
